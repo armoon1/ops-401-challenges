4d298e645ed7b288a9617cdc9cf75fb07cd18fae
SEYED MEHDI HASHEMI SOHI <92489172+armoon1@users.noreply.github.com>
2024-01-17 22:48:08 -0800

    Create file_encryption2.py

---
+++ file_encryption2.py
@@ -0,0 +1,4 @@
+# Script Name:                  File encryption
+# Author:                       SEYED MEHDI HASHEMI SOHI
+# Date of latest revision:      01/16/2024
+# Purpose:                      File encryption
