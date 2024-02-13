ecf0d8b3f0affd4f4d02363644a34077106c395a
SEYED MEHDI HASHEMI SOHI <92489172+armoon1@users.noreply.github.com>
2024-02-12 17:52:53 -0800

    Create event-logging-1.py

---
+++ event-logging-1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Script Name:                  
+# Author:                       SEYED MEHDI HASHEMI SOHI
+# Date of latest revision:      02/12/2024
+# Purpose:                      Event Logging Tool Part 1 of 3
+# Source:                       chatgpt
