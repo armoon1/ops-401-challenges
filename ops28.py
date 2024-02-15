e5ab84ac655326d4aa5468b1a07c645098b0e912
SEYED MEHDI HASHEMI SOHI <92489172+armoon1@users.noreply.github.com>
2024-02-14 17:42:31 -0800

    Create ops28.py

---
+++ ops28.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Script Name:                  
+# Author:                       SEYED MEHDI HASHEMI SOHI
+# Date of latest revision:      02/14/2024
+# Purpose:                      Event Logging Tool Part 3 of 3
+# Source:                       
