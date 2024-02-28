383344e684514c5a9c20ccf5429d02f8727f5642
SEYED MEHDI HASHEMI SOHI <92489172+armoon1@users.noreply.github.com>
2024-02-27 21:26:06 -0800

    Create ops37.py

---
+++ ops37.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Script Name:                  Cookie Capture Capades
+# Author:                       SEYED MEHDI HASHEMI SOHI
+# Date of latest revision:      02/26/2024
+# Purpose:                      Cookie Capture Capades
+# Source:                       Demo Ops 37
+
