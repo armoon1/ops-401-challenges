58790e2961e547fffcd7849d7e6d86dd3fc721e5
SEYED MEHDI HASHEMI SOHI <92489172+armoon1@users.noreply.github.com>
2024-02-28 23:22:15 -0800

    Create ops38.py

---
+++ ops38.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Script Name:                  XSS Vulnerability Detection with Python
+# Author:                       SEYED MEHDI HASHEMI SOHI
+# Date of latest revision:      02/28/2024
+# Purpose:                      Cookie Capture Capades
+# Source:                       XSS Vulnerability Detection with Python
+
