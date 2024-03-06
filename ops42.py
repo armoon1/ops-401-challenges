80f692e567defdd16326402480b26f1571498c07
SEYED MEHDI HASHEMI SOHI <92489172+armoon1@users.noreply.github.com>
2024-03-05 19:25:37 -0800

    Create ops42.py

---
+++ ops42.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Script Name:                  Attack Tools Part 2 of 3
+# Author:                       SEYED MEHDI HASHEMI SOHI
+# Date of latest revision:      03/05/2024
+# Purpose:                      Attack Tools Part 2 of 3
+# Source:                       ---
